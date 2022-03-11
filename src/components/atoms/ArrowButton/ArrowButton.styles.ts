1ab45bd2166aa8d9151fe63bcae1ab214151dbc5
Michał Gąsiorek <michalgasiorek@MacBook-Pro-Micha.local>
2022-03-12 00:25:49 +0100

    Added hover for arrows in carousel

--- src/components/atoms/ArrowButton/ArrowButton.styles.ts
+++ src/components/atoms/ArrowButton/ArrowButton.styles.ts
@@ -1,3 +1,4 @@
+import { rgba } from 'polished';
 import styled from 'styled-components';
 
 interface ArrowButtonProps {
@@ -7,7 +8,7 @@ export const StyledArrowButton = styled.button<ArrowButtonProps>`
   position: absolute;
   top: 0;
   right: ${({ dir }) => (dir === 'forward' ? '0' : 'auto')};
-  left: ${({ dir }) => (dir === 'backward' ? '0' : 'auto')};
+  left: ${({ dir }) => (dir === 'back' ? '0' : 'auto')};
   bottom: 0;
   z-index: 2;
   padding: 16px;
@@ -15,4 +16,14 @@ export const StyledArrowButton = styled.button<ArrowButtonProps>`
   border: none;
   font-size: ${({ theme }) => theme.fontSizes.m};
   color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-image: linear-gradient(
+      to ${({ dir }) => (dir === 'forward' ? 'right' : 'left')},
+      transparent,
+      ${({ theme }) => rgba(theme.colors.black, 0.1)}
+    );
+    transition: background-image 0.3s ease-in-out;
+  }
 `;
